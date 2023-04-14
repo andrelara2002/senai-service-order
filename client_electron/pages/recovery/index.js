89c98bebb1e67065e4a47a27fdfa5ac1f6610736
andrelara2002 <andrelara2002@gmail.com>
2023-04-14 13:40:53 -0300

    fix: Removing Unused and Problematic Files

--- client_electron/pages/recovery/index.js
+++
@@ -1,8 +0,0 @@
-const userApi = new UserApi()
-document.getElementById('button_login').addEventListener('click', async () => {
-    const email = document.getElementById('email').value
-
-    await userApi.recover(email, res => {
-        console.log(res)
-    })
-})
